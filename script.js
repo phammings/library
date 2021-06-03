da7806ef6fb1943986a8ab423047a3ed56c42f20
Ryan <ryanlepham9@gmail.com>
2021-06-03 16:06:13 -0400

    Checks if all fields are inputted

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const newBookButton = document.getElementById("new-book-button");
 const popupForm = document.getElementById("popup-prompt");
 const popupContainer = document.querySelector(".popup-container");
+const title = document.querySelector(".title");
+const author = document.querySelector(".author");
+const pages = document.querySelector(".pages");
+const isRead = document.querySelector(".checkbox");
+const submitButton = document.querySelector(".submit-button");
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -18,10 +24,39 @@ function displayBook() {
   });
 }
 
+submitButton.addEventListener("click", () => {
+  isAllEntriesFilled();
+  createBook();
+});
+
 newBookButton.addEventListener("click", () => {
   promptNewBookDetails();
   displayOverlay();
 });
+
+function resetFields() {
+  title.value = "";
+  author.value = "";
+  pages.value = "";
+  isRead.checked = false;
+}
+
+function isAllEntriesFilled() {
+  if (title.value === "" || author.value === "" || pages.value === "") {
+    alert("Please fill out any missing fields.");
+  } else {
+    removeOverlay();
+    resetFields();
+  }
+}
+
+function createBook() {}
+
+function removeOverlay() {
+  overlay.style.display = "none";
+  popupForm.classList.remove("popup-active");
+  popupContainer.classList.add("hidden");
+}
 
 function promptNewBookDetails() {
   popupForm.classList.add("popup-active");
@@ -29,12 +64,12 @@ function promptNewBookDetails() {
 }
 
 const overlay = document.getElementById("popup-overlay");
-overlay.addEventListener("click", () => {
-  overlay.style.display = "none";
-  popupForm.classList.remove("popup-active");
-  popupContainer.classList.add("hidden");
-});
+overlay.addEventListener("click", () => removeOverlay());
 
 function displayOverlay() {
   overlay.style.display = "block";
 }
+
+//Store user input into new book object
+//Alert user if
+//Display new book
